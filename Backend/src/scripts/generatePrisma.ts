19afe1a9026f4fc83bca861970acbb51d65a02c4
Kauã Vinícius <81399319+kaua-pt@users.noreply.github.com>
2022-11-27 14:17:04 -0300

    KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK COISA DE MALUCO ISSO AQUI TA ?

---
+++ Backend/src/scripts/generatePrisma.ts
@@ -0,0 +1,93 @@
+import prisma from "../prismaClient";
+import places from "./utils";
+
+const generateSubject = async (codigoNome: String[]) => {
+  const subjects = Array.from(codigoNome).map((el) => ({
+    codeId: el.split("-")[0].trim(),
+    name: el.split("-")[1].trim(),
+  }));
+  await prisma.subject.createMany({ data: subjects });
+};
+
+const generateClass = async (
+  codigoNome: String[],
+  matrizRef: String[],
+  nome: String[],
+  local: String[],
+  turma: String[],
+  horario: String[]
+) => {
+
+  const matrizMaterias = codigoNome.map((materia) => materia.split("-")[0].trim());
+  const teacher = nome.map((prof) => prof.split("(")[0].trim());
+
+  const room = [];
+  for (let i = 0; i < local.length; i += 1) {
+    const roomS = [];
+    for (let j = 0; j < places.length; j += 1) {
+      if (local[i].includes(places[j])) {
+        roomS.push(places[j]);
+      }
+    }
+
+    if (roomS.length === 2 && roomS[1] === "LAB NEI 2") {
+      roomS.shift();
+    }
+    room.push(roomS);
+  }
+
+  const newHorario = horario.map((horarioV: String) => horarioV.split(" "));
+  const cut = newHorario.map((sigla) => {
+    const aux = [];
+    for (let i = 0; i < sigla.length; i += 1) {
+      let index = sigla[i].indexOf("M");
+
+      if (index === -1) {
+        index = sigla[i].indexOf("T");
+      }
+      if (index === -1) {
+        index = sigla[i].indexOf("N");
+      }
+      if (index !== -1) {
+        const little = [];
+        const days = sigla[i].slice(0, index);
+        const period = sigla[i][index];
+        const hour = sigla[i].slice(index + 1);
+        little.push(days, period, hour); // dia padrao
+        aux.push(little);
+      }
+    }
+
+    return aux; // total
+  });
+
+  const hour: number[][][] = [];
+  const day: number[][][] = [];
+  for (let k = 0; k < cut.length; k += 1) {
+    const hourP = [];
+    const dayP = [];
+    for (let l = 0; l < cut[k].length; l += 1) {
+      const diasString = cut[k][l][0].split("");
+      const diasInt = diasString.map((dias) => parseInt(dias, 10));
+      dayP.push(diasInt);
+
+      // horario
+      const horariosString = cut[k][l][2].split("");
+      const horariosInt = horariosString.map((horarioV) => parseInt(horarioV, 10));
+      let horarios = [];
+      if (cut[k][l][1] === "M") {
+        horarios = horariosInt.map((numero) => numero + 7);
+      } else if (cut[k][l][1] === "T") {
+        horarios = horariosInt.map((numero) => numero + 12);
+      } else {
+        horarios = horariosInt.map((numero) => numero + 17);
+      }
+      hourP.push(horarios);
+    }
+    hour.push(hourP);
+    day.push(dayP);
+  }
+  
+};
+
+export default { generateSubject, generateClass };
--- Backend/src/scripts/scrapping.js
+++ Backend/src/scripts/scrapping.js
@@ -1,7 +1,5 @@
 const puppeteer = require("puppeteer");
 const { PrismaClient } = require("@prisma/client");
-const { convertTypeAcquisitionFromJson } = require("typescript");
-const { Console } = require("console");
 
 (async () => {
   const browser = await puppeteer.launch({ headless: true });
@@ -41,7 +39,7 @@ const { Console } = require("console");
 
   //console.log(codigoNome);
   //console.log(turma);
-  // console.log(nome);
+  //console.log(nome);
   //console.log(local);
   //console.log(horario);
   //console.log(matrizRef);
@@ -88,17 +86,11 @@ const { Console } = require("console");
     "LDTEA SALA 3",
   ];
 
-  //const prisma = new PrismaClient();
-  //prisma.$connect();
-
   // tratar dados e joga-los no banco
   const subjects = Array.from(codigoNome).map((el) => ({
     codeId: el.split("-")[0].trim(),
     name: el.split("-")[1].trim(),
   }));
-
-  //criar materias TODO
-  //const createMany = await prisma.subject.createMany({ data: subjects });
 
   //criar matriz de matérias DONE
   const matrizMaterias = codigoNome.map((materia) => {
@@ -114,13 +106,13 @@ const { Console } = require("console");
     for (i = 0; i < sigla.length; i++) {
       let index = sigla[i].indexOf("M");
 
-      if (index == -1) {
+      if (index === -1) {
         index = sigla[i].indexOf("T");
       }
-      if (index == -1) {
+      if (index === -1) {
         index = sigla[i].indexOf("N");
       }
-      if (index != -1) {
+      if (index !== -1) {
         let little = [];
         let days = sigla[i].slice(0, index);
         let period = sigla[i][index];
@@ -129,70 +121,59 @@ const { Console } = require("console");
         aux.push(little);
       }
     }
-    if (aux.length > 0) {
-      return aux; // total
-    }
+    return aux; // total
   });
+
   // cut = todos
   let hour = [];
   let day = [];
-  cut.map((conj) => {
-    let hourP = [];
-    let dayP = [];
-    conj.map((subconj) => {
-      //dias
-      let diasString = subconj[0].split("");
-      let diasInt = diasString.map((dias) => {
-        return parseInt(dias, 10);
-      });
+
+  for (k = 0; k < cut.length; k++) {
+    const hourP = [];
+    const dayP = [];
+    for (l = 0; l < cut[k].length; l++) {
+      const diasString = cut[k][l][0].split("");
+      const diasInt = diasString.map((dias) => parseInt(dias, 10));
       dayP.push(diasInt);
 
-      //horario
-      let horariosString = subconj[2].split("");
-      let horariosInt = horariosString.map((horario) => {
-        return parseInt(horario, 10);
-      });
+      // horario
+      const horariosString = cut[k][l][2].split("");
+      const horariosInt = horariosString.map((horarioV) => parseInt(horarioV, 10));
       let horarios = [];
-      if (subconj[1] == "M") {
-        horarios = horariosInt.map((numero) => {
-          return numero + 7;
-        });
-      } else if (subconj[1] == "T") {
-        horarios = horariosInt.map((numero) => {
-          return numero + 12;
-        });
+      if (cut[k][l][1] === "M") {
+        horarios = horariosInt.map((numero) => numero + 7);
+      } else if (cut[k][l][1] === "T") {
+        horarios = horariosInt.map((numero) => numero + 12);
       } else {
-        horarios = horariosInt.map((numero) => {
-          return numero + 17;
-        });
+        horarios = horariosInt.map((numero) => numero + 17);
       }
       hourP.push(horarios);
-    });
+    }
     hour.push(hourP);
     day.push(dayP);
-  });
-  console.log(hour);
-  console.log(day);
+  }
 
-  //nome professor DONE
   const teacher = nome.map((prof) => {
     return prof.split("(")[0].trim();
   });
 
-  // criar local DONE
   const room = [];
   for (i = 0; i < local.length; i++) {
     const roomS = [];
-    places.map((place) => {
-      if (local[i].includes(place)) {
-        roomS.push(place);
+    for (j = 0; j < places.length; j++) {
+      if (local[i].includes(places[j])) {
+        roomS.push(places[j]);
       }
-    });
+    }
+
     if (roomS.length == 2 && roomS[1] == "LAB NEI 2") {
       roomS.shift();
     }
     room.push(roomS);
   }
+
+  //decidir quando avançar ou nao
+  for (h = 0; h < turma.length; h++) {}
 
   await browser.close();
 })();
