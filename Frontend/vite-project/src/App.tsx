d1b010bddec03bef4938e73bafb7d5a5c089cc6f
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-11-30 16:08:13 -0300

    First frontend settings

---
+++ Frontend/vite-project/src/App.tsx
@@ -0,0 +1,15 @@
+
+import Router from './Routes'
+import GlobalStyle from './GlobalStyles/reset'
+
+function App() {
+
+  return (
+    <>
+      <Router />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
